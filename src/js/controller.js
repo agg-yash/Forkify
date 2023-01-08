05cd589739a3805b6da04580e084bb21b97a882f
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-01-08 14:51:13 +0530

    dynamic rendering of recipies using hashchange

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,10 +31,12 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd034"
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -158,4 +160,8 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+
+window.addEventListener("hashchange", showRecipe);
+window.addEventListener("load", showRecipe);
+
 console.log("Hello");
