9de4c46081ea59288695e5fc9dbe3db905bddb63
lemontree68 <aggarwalswayash.1329@gmail.com>
2023-01-09 17:44:01 +0530

    add bookmarks to local storage

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -6,6 +6,11 @@ class BookmarksView extends View {
   _parentElement = document.querySelector(".bookmarks__list");
   _errorMessage = "No bookmarks yet, find a recipe and bookmark it.";
   _message = "";
+
+  addHandlerRender(handler) {
+    window.addEventListener("load", handler);
+  }
+
   _generateMarkup() {
     return this._data
       .map((bookmark) => previewView.render(bookmark, false))
